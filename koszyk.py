e5a8e2f0e4592e34441831f54812c45d4118b79a
gosiamalgosia <gosiamalgosia@users.noreply.github.com>
2020-12-13 11:01:36 +0100

    zajecia

---
+++ koszyk.py
@@ -0,0 +1,15 @@
+koszyk = [
+    {'produkt': 'mleko', 'ilosc': 1, 'cena': 1.5},
+    {'produkt': 'czekolada', 'ilosc': 2, 'cena': 2.5},
+    {'produkt': 'konserwa', 'ilosc': 4, 'cena': 3.5}
+]
+
+suma = 0
+for poz in koszyk:
+    il = poz['ilosc']
+    c = poz['cena']
+    sum =  suma + (c * il)
+    # print(c)
+    # print(suma)
+    #print(poz)
+print(suma)
